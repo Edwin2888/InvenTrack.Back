34dedb915e0ecc92ee1f6871ce51e50983ed2e7d
edwin2888 <stiven13111@hotmail.com>
2024-03-14 21:46:28 -0500

    Se quita guards web

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,11 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // $middleware->append(VerifyJwtToken::class);
         $middleware->group('api', [
             'auth' => VerifyJwtToken::class,
         ]);
-        // $middleware->append(Illuminate\Auth\Middleware\Authenticate::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
