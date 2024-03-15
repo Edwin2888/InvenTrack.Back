d3230624c7c773bdd3766e2bf9e633564b311d40
edwin2888 <stiven13111@hotmail.com>
2024-03-14 23:17:09 -0500

    Se realiza ajuste para que las pruebas no afecte la base de datos real

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        config(['database.default' => 'sqlite']);
+        config(['database.connections.sqlite.database' => ':memory:']);
+
+        $this->artisan('migrate');
+    }
 }
