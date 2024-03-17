d3c31fb21ddc52f5d014423982dca2322bd11239
edwin2888 <stiven13111@hotmail.com>
2024-03-17 01:27:49 -0500

    Ajuste test

--- tests/Feature/JwtAuthTest.php
+++ tests/Feature/JwtAuthTest.php
@@ -11,17 +11,18 @@ class JwtAuthTest extends TestCase
 {
     public function test_registrar_usuario(): void {
         Artisan::call('migrate');
+        $urlBASE = 'API/auth/registrarUsuario';
 
-        $registroErrorMailIncorrecto = $this->post('api/auth/registrarUsuario', ['email' => 'email.incorrecto', 'name' => 'usuario', 'password' => '123456']);
+        $registroErrorMailIncorrecto = $this->post($urlBASE, ['email' => 'email.incorrecto', 'name' => 'usuario', 'password' => '123456']);
         $registroErrorMailIncorrecto->assertStatus(400)->assertSee(['email', 'valid email']);
 
-        $registroErrorNameRequerido = $this->post('api/auth/registrarUsuario', ['email' => 'email@correcto.com', 'password' => '123456']);
+        $registroErrorNameRequerido = $this->post($urlBASE, ['email' => 'email@correcto.com', 'password' => '123456']);
         $registroErrorNameRequerido->assertStatus(400)->assertSee(['name', 'required']);
 
-        $registroErrorNameRequerido = $this->post('api/auth/registrarUsuario', ['email' => 'email@correcto2.com', 'name' => 'usuario', 'password' => '123']);
+        $registroErrorNameRequerido = $this->post($urlBASE, ['email' => 'email@correcto2.com', 'name' => 'usuario', 'password' => '123']);
         $registroErrorNameRequerido->assertStatus(400)->assertSee(['password', '6']);
 
-        $registroConExito = $this->post('api/auth/registrarUsuario', ['email' => 'email@correcto3m.com', 'name' => 'usuario1', 'password' => '1234567']);
+        $registroConExito = $this->post($urlBASE, ['email' => 'email@correcto3m.com', 'name' => 'usuario1', 'password' => '1234567']);
         $registroConExito->assertStatus(200)->assertSee(['token', 'usuario']);
     }
 }
